a8ac0dd4589e438908a3a37cef59d5cb18648d12
Jodie Alaine Parker <me@jodiealaine.com>
2014-11-21 16:33:15 +0000

    Implement autoload() for mail_form/base

--- test/mail_form_test.rb
+++ test/mail_form_test.rb
@@ -4,7 +4,7 @@ require "fixtures/sample_mail"
 class MailFormTest < ActiveSupport::TestCase
   test "sample mail has name and email as attributes" do
     sample = SampleMail.new
-    sample.name - "User"
+    sample.name = "User"
     assert_equal "User", sample.name
     sample.email = "user@example.com"
     assert_equal "user@example.com", sample.email
